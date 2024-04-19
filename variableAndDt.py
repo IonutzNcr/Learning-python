9f7f8a7dde53495279d64b8926547b71cc1226c8
Ionut N <ionut.nicoara@epitech.eu>
2024-04-19 17:17:56 +0200

    variable and simple data type

---
+++ variableAndDt.py
@@ -0,0 +1,25 @@
+#variables et type de donné
+
+# numbers
+a = 4
+b = 4.3
+# c = "3" + 4j  erreur concar string with complex
+
+# type complex 
+c = 3 + 4j + 200
+print(type(c))
+print(c)
+
+# bool attention value start with capital
+d = False
+print(d)
+
+
+# nonetype also start with a capital
+e = None 
+print(e)
+
+f = "françois"
+print(f)
+
+
